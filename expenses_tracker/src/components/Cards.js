3e9eacc9c0275326312f23b69be407c276c1b931
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-04-22 13:09:51 +0530

    added expenses tracker project

---
+++ expenses_tracker/src/components/Cards.js
@@ -0,0 +1,9 @@
+import "./Cards.css";
+
+function Cards(props) {
+  const classes = "card " + props.className;
+
+  return <div className={classes}>{props.children} </div>;
+}
+
+export default Cards;
