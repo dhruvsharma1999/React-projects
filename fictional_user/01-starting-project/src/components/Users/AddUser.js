cedbe62cae134c76d7d568b46e6d1eabdb37c0ef
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-05-12 15:11:17 +0530

    added react portal

--- fictional_user/01-starting-project/src/components/Users/AddUser.js
+++ fictional_user/01-starting-project/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
   const [enteredName, setEnteredName] = useState("");
@@ -39,7 +40,7 @@ const AddUser = (props) => {
     setError(null);
   };
   return (
-    <>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -66,7 +67,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </>
+    </Wrapper>
   );
 };
 
