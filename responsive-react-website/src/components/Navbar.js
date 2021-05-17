39fe0b901ed89ce39d48c9967e6547a11d759de9
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-05-17 20:08:49 +0530

    added router and icons

--- responsive-react-website/src/components/Navbar.js
+++ responsive-react-website/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBeer, FaTypo3 } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -7,7 +8,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TRVL <i class="fab fa-typo3"></i>
+            TRVL <FaTypo3 />
           </Link>
         </div>
       </nav>
