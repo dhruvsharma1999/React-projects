70da494b64e743ed4f80c69ebcc57fde3c070ffd
dhruvsharma1999 <dhruvshrm34@gmail.com>
2021-06-10 00:25:55 +0530

    added fake json data api for adding and deleting tasks

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
 import AddTask from "./Components/AddTask";
@@ -7,35 +7,54 @@ import "./index.css";
 function App() {
   const [showForm, setShowForm] = useState(false);
   //task setting state
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+  //Setting useEffect hook for fetching the data from the localhost server
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTask();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //Fetch task from local host
+  const fetchTask = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    return data;
+  };
 
   // add a task
-  const addTask = (newTask) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newTaskWithID = { id, ...newTask };
-    setTasks([...tasks, newTaskWithID]);
+  const addTask = async (newTask) => {
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // const newTaskWithID = { id, ...newTask };
+    // setTasks([...tasks, newTaskWithID]);
+
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    });
+
+    const data = res.json();
+
+    setTasks([...Tasks, data]);
   };
 
   //delte a task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
     //dont' show the task in the task state whose id are not similar to id of delete task func called in Task componenet
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const onClickReminder = (id) => {
+  const onClickReminder = async (id) => {
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
@@ -44,14 +63,14 @@ function App() {
   };
 
   const formControlHandeler = () => {
-    setShowForm(!showForm);
+    setShowForm(!showForm); // toggeling the initial state of show form state component
   };
 
   return (
     <div className="container">
       <Header
-        title="Task Tracker"
-        onToggle={formControlHandeler}
+        title="Task Tracker" //Passing title prop to set the title of header component
+        onToggle={formControlHandeler} //passing onToggle prop, which is calling formControlHandeler on click
         showForm={showForm}
       />
       {showForm && <AddTask onAdd={addTask} />}
--- task-tracker/src/Components/Header.js
+++ task-tracker/src/Components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "./Button";
 
 const Header = ({ title, onToggle, showForm }) => {
+  //Header component to render the header and task adding functionality
   return (
     <header className="header">
       <h1>{title}</h1>
